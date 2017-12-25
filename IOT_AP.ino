4129aef98874d170a84541c0b8d89c0bf20d98d6
Sarfaraj hussain <sarfaraj118@gmail.com>
2017-12-25 12:44:28 +0530

    Add files via upload
    IOT _AP is router code which you can sent data to server using quectel module ec25-e.
    and _20_mesh is client code you can flash all client node this will auto and intelligent manage in the mesh way !

---
+++ IOT_AP.ino
@@ -0,0 +1,308 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+extern "C" {
+#include "user_interface.h"
+}
+// IoT platform Credentials
+String apiKey = "DCU";
+//const char* logServer = "117.218.35.237";
+const char* logServer = "103.208.152.191";
+String data;
+// Internet router credentials
+const char* ssid = "DCU";
+const char* password = "india";
+int Status=1;
+ESP8266WebServer server(80);
+String serv_ack ;
+String tmp ;
+int  ACK_SERV, kk,jj = 0;
+const byte numChars = 32;
+char receivedChars[numChars]; // an array to store the received data
+boolean newData = false;
+/**************************XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX************************************************/
+void setup() {
+Serial.begin(115200);
+Serial.println("");
+Serial.println("Remember to to set Both NL & CR in the serial monitor.");
+Serial.println("Ready");
+Serial.println(""); 
+//WiFi.mode(WIFI_AP_STA);
+WiFi.mode(WIFI_AP);
+struct softap_config config;
+wifi_softap_get_config(&config); // Get config first.
+config.max_connection = 8;
+
+setupAccessPoint();
+GSM_MODULE_INIT();TCP_INIT();
+}
+void client_status() {
+
+unsigned char number_client;
+struct station_info *stat_info;
+
+struct ip_addr *IPaddress;
+IPAddress address;
+int i=1;
+
+number_client= wifi_softap_get_station_num();
+yield();
+stat_info = wifi_softap_get_station_info();
+Serial.print("Total connected_client are = ");
+Serial.println(number_client);
+while (stat_info != NULL) 
+{
+IPaddress = &stat_info->ip;
+address = IPaddress->addr;
+
+Serial.print("client= ");
+
+Serial.print(i);
+Serial.print(" ip address is = ");
+Serial.print((address));
+Serial.print(" with mac address is = ");
+
+Serial.print(stat_info->bssid[0],HEX);
+Serial.print(stat_info->bssid[1],HEX);
+Serial.print(stat_info->bssid[2],HEX);
+Serial.print(stat_info->bssid[3],HEX);
+Serial.print(stat_info->bssid[4],HEX);
+Serial.println(stat_info->bssid[5],HEX);
+stat_info = STAILQ_NEXT(stat_info, next);
+i++;
+}delay(300);
+}
+
+
+
+// Handling the / root web page from my server 
+void handle_index() {
+server.send(200, "text/plain", "WELCOME IN DCU WORLD !!");
+}
+
+// Handling the /feed page from my server
+void  handle_feed(){
+Status= 0;
+String A = server.arg("packet-A");
+String B = server.arg("packet-B");
+String C = server.arg("packet-C");
+String D = server.arg("packet-D");
+
+
+tmp=A;
+server.send(200, "text/plain", "HELLO");
+Serial.println(A);
+Serial.println(B);
+Serial.println(C);
+Serial.println(D);
+
+setupStMode(A,B,C,D);
+
+}
+
+void setupAccessPoint(){
+Serial.println("** SETUP ACCESS POINT **"); 
+client_status() ; 
+Serial.println("- disconnect from any other modes");
+//WiFi.disconnect();
+Serial.println("- start ap with SID: "+ String(ssid));
+WiFi.softAP(ssid, password);
+IPAddress myIP = WiFi.softAPIP();
+Serial.print("- AP IP address is :");
+Serial.print(myIP);
+setupServer();
+}
+
+void setupServer(){
+
+Serial.println("/r/n** setupServer() **/r/n");
+Serial.println("** SETUP SERVER **");
+Serial.println("- starting server :");
+server.on("/", handle_index);
+server.on("/feed", handle_feed);
+server.begin();
+
+};
+
+//void setupStMode(String t, String v){
+/*************************************************************************************/
+void setupStMode(String A,String B,String C,String D){
+Serial.println("*************** SETUP STATION MODE**************************** **");
+//Serial.begin(115200);Serial.swap();
+TCP_ACTIVATION(); TCP_CONN_ESTABLISED();TCP_DATA_TX();//TCP_DATA_RX();
+
+Serial.println();Serial.println("- succesfully connected");Serial.println("- starting client");
+WiFiClient client;
+
+//Read what the browser has sent into a String class and print the request to the monitor
+//String request = client.readString();
+
+//Serial.println(request);
+//Serial.println("- SIDE-1 : "+String(t));
+//Serial.println("- SIDE-2 : "+String(v));
+Serial.println("- connecting to Database server: " + String(logServer));
+client.stop();
+Serial.println("- stopping the client");
+Status= 1;
+client_status();
+Serial.println("**************************XXXXXXXXXXX!!!E---N---D!!!XXXXXXXXXXXXXXX************************************************");
+Serial.println("");
+
+}
+
+
+
+/****************************QUECTEL-EC-25_Module*****************************************/
+
+void GSM_MODULE_INIT()   /*GSM_MODULE initilization*/
+{ 
+   int i,  num =11; 
+  char *GSM_MODULE_INIT[11] ={"ATV1","ATE0","AT+CMEE=2","AT+IPR=115200;&W","AT+QURCCFG=\"URCPORT\",\"uart1\"","AT+CPIN?","AT+CIMI","AT+QCCID","AT+CSQ","AT+CREG?"};
+
+ for (i = 0; i <num; i++) 
+{
+Serial.println(GSM_MODULE_INIT[i]);
+//uartreceive();
+recvWithEndMarker();showNewData();
+}}
+void TCP_INIT()
+{ 
+int i,  num =3; 
+char *TCP_INIT[3] = {"AT+CPIN?","AT+CREG?","AT+CSQ"} ;
+for (i = 0; i <num; i++) 
+  { Serial.println(TCP_INIT[i]); 
+recvWithEndMarker();showNewData();//uartreceive();
+}
+  }
+void TCP_ACTIVATION()
+{
+int i,  num =5; 
+char *TCP_ACTIVATION[5] = {"AT+CPIN?","AT+CREG?","AT+QICSGP=1,1,\"UNINET\",\"\",\"\",0","AT+QIACT=1","AT+QIACT?"} ;
+for (i = 0; i < num; i++) 
+   {
+ Serial.println(TCP_ACTIVATION[i]); delay(500);
+recvWithEndMarker();showNewData();//uartreceive();
+}}
+
+ void TCP_CONN_ESTABLISED()
+{
+int i,  num =1; 
+char *TCP_CONN_ESTABLISED[1] = {"AT+QIOPEN=1,0,\"TCP\",\"103.208.152.191\",587,0,1"} ;//103.208.152.191//117.218.35.237
+for (i = 0; i < num; i++) 
+{
+Serial.println(TCP_CONN_ESTABLISED[i]); delay(200);
+recvWithEndMarker();showNewData();//uartreceive();
+
+delay(500);}}   
+
+void TCP_DATA_TX()
+{
+int i,  num =6; 
+char *TCP_DATA_TX[6] = {"AT+QISEND=0","","\x1A","AT+QISEND=0,0","AT+QISEND=0,0","AT+QIRD=0,0"} ;
+for (i = 0; i < num; i++) 
+{
+if(i==1)
+{ Serial.println(tmp);
+i++;}
+Serial.println(TCP_DATA_TX[i]);delay(300);
+if (i==2)
+{ACK_SERV =1;}
+recvWithEndMarker();showNewData();//uartreceive();
+//delay(500); 
+}}
+
+void TCP_DATA_RX()
+{
+int i,  num =2; 
+char line[20];
+char *TCP_DATA_RX[2] = {"AT+QIRD=0,0","AT+QIRD=0,0"} ;
+
+for (i = 0; i < num; i++) 
+{    
+  Serial.println(TCP_DATA_RX[i]); 
+  delay(500);
+recvWithEndMarker();showNewData();//uartreceive();
+//i++;
+
+delay(500);
+}
+}
+
+
+
+/********************ACKNLOGEMENT-MECHANISM***************************************/
+void recvWithEndMarker() {
+
+ static byte ndx = 0;
+ char endMarker = '\n';
+ char recv;
+ char Q_S_ACK[9]="SEND OK",Q_SER_ACK[9]="HELLO";
+ int i;
+ // if (Serial.available() > 0) {
+           while (Serial.available() > 0 ) {
+ recv = Serial.read();
+
+ if (recv != endMarker) {
+ receivedChars[ndx] = recv;
+// Serial.println(ndx);
+// Serial.println(recv);
+ ndx++;
+ if(ACK_SERV=1)
+ {
+  if(((Q_SER_ACK[0]) ==(receivedChars[0]))&&((Q_SER_ACK[1]) ==(receivedChars[1]))&&((Q_SER_ACK[2]) ==(receivedChars[2]))&&
+ ((Q_SER_ACK[3]) ==(receivedChars[3]))&&((Q_SER_ACK[4]) ==(receivedChars[4])))
+ {Serial.println("!!!!!!!!!!!!!!!!!!!!!!HELLOOOO RECEIVED FROM SERVER !!!!!!!!!!!!!!!!!!!!!!!!!!");
+  server.send(200, "text/plain", "DONE!");}
+ 
+ }
+ 
+ if (((Q_S_ACK[0]) ==(receivedChars[0]))&&((Q_S_ACK[1]) ==(receivedChars[1]))&&((Q_S_ACK[2]) ==(receivedChars[2]))&&
+ ((Q_S_ACK[3]) ==(receivedChars[3]))&&((Q_S_ACK[4]) ==(receivedChars[4]))&&((Q_S_ACK[5]) ==(receivedChars[5]))&&((Q_S_ACK[6]) ==(receivedChars[6])))
+{
+  Serial.println("!!!!!!!!!!!!!!!!!!!!!!DATA SENT TO SERVER !!!!!!!!!!!!!!!!!!!!!!!!!!");
+ // server.send(200, "text/plain", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx !");
+  delay(500);
+  }
+ if (ndx >= numChars) {
+ ndx = numChars - 1;
+ }
+ }
+ else {
+ receivedChars[ndx] = '\0'; // terminate the string
+ 
+ ndx = 0;
+ newData = true;
+ }
+ }
+}
+
+void showNewData() {
+ while (newData == true) {
+ //Serial.print("This just in ... ");
+ Serial.println(receivedChars);
+  newData = false;
+
+ }
+}
+/**********************************************************************************************/
+//void sendHttpRequest() {
+//HTTPClient http;
+////  http.begin(serverHost);
+//http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+//http.POST(data);
+//http.writeToStream(&Serial);
+//http.end();
+//
+//}
+
+void loop() {
+  if (Status == 1)
+  {    server.handleClient();
+ 
+  }
+  else
+  Serial.println("AT");
+  
+}
---
+++ _20_MESH-TEST.ino
@@ -0,0 +1,291 @@
+/*
+Geekstips.com
+IoT project - Communication between two ESP8266 - Talk with Each Other
+ESP8266 Arduino code example
+*/
+//#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFiMesh.h>
+
+extern "C" {
+#include "user_interface.h"
+}
+// AP Wi-Fi credentials level wise!
+const char* ssid = "DCU";
+const char* password = "india";
+const char* ssid1 = "MESH";
+const char* password1 = "india";
+//const char* ssid2 = "MESH1";
+//const char* password2 = "india";
+const char* logServer = "192.168.4.1";
+const char* logServer1 = "192.167.8.6";
+//const char* logServer2 = "192.169.7.61";
+// Local ESP web-server address level wise!
+String serverHost = "http://192.168.4.1/feed";
+String serverHost_ack ="http://192.168.4.1/response";
+String serverHost1 = "http://192.167.8.6/feed";
+String serverHost_ack1 ="http://192.167.8.6/response";
+//String serverHost2 = "http://192.169.7.6/feed";
+//String serverHost_ack2 ="http://192.169.7.6/response";
+String data;
+// DEEP_SLEEP Timeout interval
+int sleepInterval = 5;
+// DEEP_SLEEP Timeout interval when connecting to AP fails
+int failConnectRetryInterval = 2;
+int counter = 0,rou_x=0,Status=1,reset =0;
+char *X;
+char *m;
+String A,B,C,D,E,F,G ;
+// Static network configuration
+//IPAddress ip(192, 168, 4, 4);
+//IPAddress gateway(192, 168, 4, 1);
+//IPAddress subnet(255, 255, 255, 0);
+IPAddress L1_IP(192, 167, 8, 6);//level 1
+IPAddress L2_IP(192, 169, 7, 6);//level 2
+IPAddress L3_IP(192, 162, 4, 6);//level 2
+WiFiClient client;
+ESP8266WebServer server(80);
+
+void setup() {
+  ESP.eraseConfig();
+  WiFi.persistent(false);
+  Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println("******** BEGIN-1 ***********");
+  Serial.print("MAC-ADDRESS:");Serial.println(WiFi.macAddress());
+   delay(500);
+  Serial.println("- set ESP STA mode");
+  WiFi.mode(WIFI_AP_STA);
+  Serial.println("- connecting to wifi");
+//  WiFi.config(ip, gateway, subnet); 
+   
+     
+/*********/
+connection_init();
+//WiFi.softAPdisconnect(false);
+  /******************/
+
+if (WiFi.SSID()== "DCU"){rou_x=0;WiFi.softAPConfig(L1_IP, L1_IP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("MESH", "india");Serial.println("-OPEN-MESH-NETWORK");}
+else if (WiFi.SSID()== "MESH"){rou_x=1;WiFi.softAPConfig(L2_IP, L2_IP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("MESH1", "india");Serial.println("- OPEN-MESH1-NETWORK");}
+else if(WiFi.SSID()== "MESH1"){rou_x=2;WiFi.softAPConfig(L3_IP, L3_IP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("MESH2", "india",1,true);Serial.println("- OPEN-MESH2-NETWORK");}
+
+  Serial.println("- wifi connected");
+  Serial.println("- read DHT sensor");
+  readDHTSensor();
+  Serial.println("- build DATA stream string");
+  buildDataStream();
+  Serial.println("- send GET request");
+  sendHttpRequest();
+  Serial.println();
+  Serial.println("***X*******X*******X******X***");
+ //  hibernate(sleepInterval);
+}
+void connection_init()
+{Stop: Serial.println("LET ME RETRYING...OUT FROM NETWORK SEARCHING MODE___!!!!");
+WiFi.softAPdisconnect(true);
+  /*****switch network*******/
+Serial.println(" ");
+Serial.println("Level-1-Trying to Connect DCU");
+while (WiFi.status() != WL_CONNECTED) {
+WiFi.begin(ssid, password);
+
+if(counter > 20){
+    Serial.println(" ");
+    Serial.println("Level-2-Trying to Connect MESH"); while (WiFi.status() != WL_CONNECTED){
+    WiFi.begin(ssid1, password1);
+            if(counter > 40){
+                                                  
+             counter=0; goto Stop;              
+          }
+          else if(reset >60){reset=0;ESP.reset();} 
+   
+    delay(500);
+    Serial.print(".");
+    counter++;reset++;
+}}
+delay(500);
+Serial.print(".");
+counter++;reset++;
+}}
+/****RECEIVING*******/
+void handle_index() {
+server.send(200, "text/plain", "PING SUCCESSFULL..!!");
+};
+void espreset() {
+server.send(200, "text/plain", "RESET SUCCESSFULL..!!");
+ESP.reset();
+};
+void  handle_feed(){ Serial.println("******ENTER HANDLE MODE*********");
+Status= 0;
+A = server.arg("packet-A");
+B = server.arg("packet-B");
+C = server.arg("packet-C");
+D = server.arg("packet-D");
+E = server.arg("packet-E");
+F = server.arg("packet-F");
+G = server.arg("packet-G");
+server.send(200, "text/plain", "THANK YOU!");
+delay(100);
+Serial.println(A);
+Serial.println(B);
+Serial.println(C);
+Serial.println(D);
+Serial.println(E);
+Serial.println(F);
+Serial.println(G);
+
+
+//server.send(200, "text/plain", "THANK YOU!");
+
+
+delay(500);
+//client_status() ;
+};
+
+void setupAccessPoint(){
+Serial.println("** SETUP ACCESS POINT **");  
+Serial.println("- disconnect from any other modes");
+WiFi.disconnect();
+IPAddress myIP = WiFi.softAPIP();
+Serial.print("- AP IP address is :");
+Serial.println(myIP);
+setupServer();
+}
+
+void setupServer(){
+Serial.println("- starting server :");
+server.on("/", handle_index);
+server.on("/feed", handle_feed);
+server.on("/espreset", espreset);
+server.begin();
+};
+
+/******************/
+
+
+
+void sendHttpRequest() { Serial.println("ENTER IN- sendHttpRequest");
+  if((WiFi.status() != WL_CONNECTED)||(WiFi.status() == WL_NO_SHIELD)||(WiFi.status() == WL_NO_SSID_AVAIL)||(WiFi.status() == WL_DISCONNECTED)||
+  (WiFi.status() == WL_CONNECTION_LOST)||(WiFi.status() == WL_CONNECT_FAILED) )
+  {Serial.println("WI-FI NOT AVALIABLE !!!");connection_init() ;}
+ 
+HTTPClient http;
+if(rou_x==0){ Serial.println("ENTER DCU HOSTING");http.begin(serverHost);}
+else if(rou_x==1){ Serial.println("ENTER MESH HOSTING");http.begin(serverHost1);}
+
+
+Serial.print("Connected with:");Serial.println(WiFi.SSID());
+client_status();
+Serial.println("--------sending data----------");
+http.POST(data);
+
+//if (rou_x==0){Serial.println("ENTER DCU ACK wait");http.begin(serverHost);}
+//else if (rou_x==1){Serial.println("ENTER MESH1 ACK wait");http.begin(serverHost1);}
+//else if (rou_x==2){Serial.println("ENTER MESH2 ACK wait");http.begin(serverHost2);}
+String payload = http.getString(); 
+Serial.println(payload);
+Serial.println(data);
+http.end();
+delay(100);
+//http.writeToStream(&Serial);
+Status= 1;
+client.flush();
+data.remove(0,data.length());A.remove(0,A.length());B.remove(0,B.length());   //||data1.length()||data2.length()||A||B||C);
+Serial.print("After clear data lenght:");
+Serial.println(data.length());
+Serial.println(A.length());
+Serial.println(B.length());
+}         
+
+void readDHTSensor() {Serial.println("ENTER IN-readDHTSensor()");
+  delay(200);
+//  X =WiFi.macAddress(); //"*DCU5001,4B,QQQQQQQQQQQQQ,000299.01,0006.6300,908728.46,558383.63,01,196.1,000.0,344749.00,0000,9E34$$";
+  //PPPPPPPPP//MMMMMMMMMMM//ZZZZZZZZZZZ//KKKKKKKKKK//SSSSSSSSSSSSS//FFFFFFFFFFFF//QQQQQQQQQQQQQ
+
+    Serial.println("- MY DATA-: "+String(X));//Serial.println(A);
+}
+
+void buildDataStream() { Serial.println("ENTER IN-buildDataStream()");
+      data = "packet-A=MY DATA:";
+      data += String(WiFi.macAddress());
+      data += "&packet-B=";
+      data += String(A);
+      data += "&packet-C=";
+      data += String(B);
+      data += "&packet-D=";
+      data += String(C);
+      data += "&packet-E=";
+      data += String(D);
+      data += "&packet-F=";
+      data += String(E);
+      data += "&packet-G=";
+      data += String(F);
+  Serial.println("- data stream: "+data);
+}
+
+
+void hibernate(int pInterval) {
+  WiFi.disconnect();
+  ESP.deepSleep(10 * 600000 * pInterval, WAKE_RFCAL);
+  delay(100);
+}
+/***********CONNECTING DEVICES STATUS***************/
+void client_status() {
+
+unsigned char number_client;
+struct station_info *stat_info;
+struct ip_addr *IPaddress;
+IPAddress address;
+int i=0;
+
+number_client= wifi_softap_get_station_num();
+stat_info = wifi_softap_get_station_info();
+
+Serial.print(" Total connected_client are = ");
+Serial.println(number_client);
+
+while (stat_info != NULL) {
+
+IPaddress = &stat_info->ip;
+address = IPaddress->addr;
+
+Serial.print("client= ");
+Serial.print(i);
+Serial.print(" ip adress is = ");
+Serial.print((address));
+Serial.print(" with mac adress is = ");
+
+Serial.print(stat_info->bssid[0],HEX);
+Serial.print(stat_info->bssid[1],HEX);
+Serial.print(stat_info->bssid[2],HEX);
+Serial.print(stat_info->bssid[3],HEX);
+Serial.print(stat_info->bssid[4],HEX);
+Serial.print(stat_info->bssid[5],HEX);
+
+stat_info = STAILQ_NEXT(stat_info, next);
+i++;
+Serial.println();
+
+}delay(500);
+}
+/************************************/
+
+void loop() 
+{Serial.println("ENTER IN- MAIN loop");
+buildDataStream();
+sendHttpRequest();
+delay(10000);
+// delay(1000);delay(1000);delay(1000);delay(1000);delay(1000);
+if (Status == 1){
+server.handleClient();
+setupServer();}
+delay(3000);
+  
+  
+  }
